a030da60ff6d30910111ba35166f28446f5eb6e5
matt.rstat@gmail.com <matt.rstat@gmail.com>
2018-08-31 15:45:55 -0600

    Add files.

---
+++ nwtapp/server.R
@@ -0,0 +1,374 @@
+eb <- element_blank()
+theme1 <- theme(plot.background=eb, legend.position="bottom")
+theme_blank <- theme(axis.line=eb, axis.text.x=eb, axis.text.y=eb, axis.ticks=eb, axis.title.x=eb, axis.title.y=eb,
+  legend.position="none", panel.background=eb, panel.border=eb, panel.grid.major=eb, panel.grid.minor=eb, plot.background=eb)
+acm_defaults <- function(map, x, y) addCircleMarkers(map, x, y, radius=6, color="black", fillColor="orange", fillOpacity=1, opacity=1, weight=2, stroke=TRUE, layerId="Selected")
+
+shinyServer(function(input, output, session) {
+  # setup
+  Monthly <- reactive({ input$toy %in% month.abb })
+  Mos <- reactive({ if(Monthly()) input$toy else month.abb[sea.idx[[input$toy]]] })
+  mon_index <- reactive({ match(Mos(), month.abb) })
+  Variable <- reactive({ vars[var.labels==input$variable] })
+  RCPs <- reactive({ rcps[rcp.labels==input$rcp] })
+
+  dt_maps_file <- reactive({
+    req(Variable(), RCPs(), input$dec)
+    paste0("map_subtables/", Variable(),"_",RCPs(), "_", input$dec, ".RData")
+  })
+
+  rv <- reactiveValues()
+
+  load_file <- function(file){
+    load(file[1], envir=environment())
+    rv[["d"]] <- d
+  }
+
+  updateData <- reactive({ load_file(dt_maps_file()) })
+  observe({ updateData() })
+
+  Extent <- reactive({
+    x <- input$lon_range
+    y <- input$lat_range
+    re <- extent(r)
+    e <- extent(c(x, y))
+    if(is.null(raster::intersect(e, re))) return(re)
+    if(length(which(!is.na(crop(r, e)[]))) < 3) return(re)
+    if(!is.null(shp())){
+      e2 <- raster::intersect(e, extent(shp()$shp))
+      if(is.null(e2)){
+        return(re)
+      } else {
+        if(length(which(!is.na((crop(r, e2) %>% mask(shp()$shp))[]))) < 3) return(re)
+      }
+    }
+    e
+  })
+
+  sea_func <- reactive({ if(Variable()=="pr") sum else mean })
+
+  stat_func <- reactive({
+    p <- input$mod_or_stat
+    if(p %in% models) return()
+    switch(p, Mean=mean,  Min=function(x,...) min(x,...), Max=function(x,...) max(x,...),
+      Spread=function(x,...) range(x,...))
+  })
+
+  # MODULE: polygon shapefile upload and related reactive objects
+  shp <- callModule(shpPoly, "user_shapefile", r=r)
+
+  Shp_plot_ht <- reactive({
+    if(is.null(shp())) return(0)
+    e <- extent(shp()$shp_original)[]
+    round(100*(e[4]-e[3])/(e[2]-e[1]))
+  })
+
+  output$Shp_Plot <- renderPlot({
+    if(!is.null(shp())){
+      cl <- class(shp()$shp_original)
+      if(cl=="SpatialPointsDataFrame"){
+        d <- data.frame(shp()$shp_original@coords, group=1)
+        names(d) <- c("long", "lat", "group")
+      } else d <- fortify(shp()$shp_original)
+      g <- ggplot(d, aes(x=long, y=lat, group=group)) + coord_equal() + theme_blank
+      if(cl=="SpatialPolygonsDataFrame"){
+        g <- g + geom_polygon(fill="steelblue4") + geom_path(colour="gray20")
+        if("hole" %in% names(d)) g <- g + geom_polygon(data=filter(d, hole==TRUE), fill="white")
+      } else if(cl=="SpatialLinesDataFrame"){
+        g <- g + geom_path(colour="steelblue4", size=2)
+      } else {
+        g <- g + geom_point(colour="steelblue4", size=2)
+      }
+    }
+    g
+  }, height=function() Shp_plot_ht(), bg="transparent")
+
+  output$Mask_in_use <- renderUI({ if(is.null(shp())) h4("None") else plotOutput("Shp_Plot", height="auto") })
+
+  output$Shp_On <- renderUI({ if(is.null(shp())) br() else checkboxInput("shp_on", "Shapefile active", TRUE) })
+
+  # prepping GCM/CRU, raw/deltas, months/seasons, models/stats, temp/precip
+  CRU_ras <- reactive({
+    idx <- match(input$toy, names(cru6190[[Variable()]]))
+    subset(cru6190[[Variable()]], idx) %>% crop(Extent())
+  })
+
+  ras <- reactive({
+    #dec.idx <- seq_along(unique(rv$d$Decade)) #which(decades==input$dec)
+    mon.idx <- switch(input$toy, Winter=c(1,2,12), Spring=3:5, Summer=6:8, Fall=9:11)
+    mo <- if(Monthly()) mon_index() else mon.idx
+    p <- input$mod_or_stat
+    if(p %in% models){
+      x <- filter(rv$d, Model==p)$Maps[[1]][[1]] %>% subset(mo) %>% crop(Extent())
+      if(!Monthly()) x <- calc(x, sea_func()) %>% round(1)
+      if(input$deltas & Variable()=="pr"){
+        x <- round(x / CRU_ras(), 2)
+        x[is.infinite(x)] <- NA
+      }
+      if(input$deltas & Variable()=="tas") x <- x - CRU_ras()
+    } else {
+      x <- group_by(rv$d, Model, add=T) %>% do(., Maps=subset(.$Maps[[1]][[1]], mo) %>% crop(Extent()) %>% calc(sea_func()))
+      x <- stat_func()(brick(x$Maps))
+      if(p=="Spread") x <- calc(x, function(x) x[2]-x[1])
+      x <- round(x, 1)
+    }
+    if(!is.null(shp()) && (is.null(input$shp_on) || input$shp_on)){
+      if(!is.null(raster::intersect(extent(x), extent(shp()$shp)))){
+        x.masked <- try(crop(x, shp()$shp) %>% mask(shp()$shp), TRUE)
+        if(!(class(x.masked)=="try-error") && length(which(!is.na(x.masked[]))) > 2) x <- x.masked
+      }
+    }
+    x[is.nan(x)] <- NA
+    x
+  })
+
+  # store raster values once, separate from raster object
+  ras_vals <- reactive({ values(ras()) })
+
+  output$dl_raster <- downloadHandler(
+    filename="rasterLayer.tif",
+    content=function(file){
+      rasfile <- writeRaster(ras(), file, format="GTiff", overwrite=TRUE)
+      file.rename(rasfile@file@name, file)
+    }
+  )
+
+  # Colors and color palettes
+  Colors <- reactive({
+    req(input$colpal)
+    pal <- input$colpal
+    custom.colors <- c(input$col_low, input$col_med, input$col_high)
+    if(pal %in% sq) custom.colors <- custom.colors[c(1,3)]
+    if(pal %in% c("Custom div", "Custom seq")) custom.colors else pal
+  })
+
+  pal <- reactive({
+    colorNumeric(Colors(), ras_vals(), na.color="transparent")
+  })
+
+  # Map legend title, also used for spatial summary density plot
+  Legend_Title <- reactive({
+    p <- input$variable=="Precipitation"
+    d <- input$deltas
+    if(p & d) "Precip. deltas" else if(p) "Precipitation (mm)" else if(!p & d) "Temp. deltas (C)" else "Temperature (C)"
+  })
+
+  # Initialize map
+  output$Map <- renderLeaflet({
+    leaflet() %>% setView(lon, lat, 4) %>% addTiles() %>%
+      addCircleMarkers(data=locs, radius=6, color="black", stroke=FALSE, fillOpacity=0.5, group="locations", layerId = ~loc)
+  })
+
+  #### Map-related observers ####
+  observe({ # raster layers
+    leafletProxy("Map") %>% removeTiles(layerId="rasimg") %>% addRasterImage(ras(), colors=pal(), opacity=0.8, layerId="rasimg")
+  })
+
+  observe({ # user-provided points
+    if(class(shp()$shp)=="SpatialPointsDataFrame" & (!is.null(input$shp_on) && input$shp_on)){
+      x <- shp()$shp@coords
+      v <- raster::extract(ras(), x)
+      x <- data.frame(x)
+      names(x) <- c("lon", "lat")
+      x$clrs <- colorNumeric(Colors(), v, na.color="transparent")(v)
+      leafletProxy("Map") %>% clearGroup("user_points") %>%
+        addCircleMarkers(data=x, weight=1, radius=6, color=~clrs, stroke=TRUE, fillOpacity=0.8, group="user_points", layerId=paste0("user_points_", 1:nrow(x)))
+    } else leafletProxy("Map") %>% clearGroup("user_points")
+  })
+
+  observe({ # legend
+    proxy <- leafletProxy("Map")
+    proxy %>% clearControls()
+    if (input$legend) {
+      proxy %>% addLegend(position="bottomright", pal=pal(), values=ras_vals(), title=Legend_Title())
+    }
+  })
+
+  observe({ # show or hide location markers
+    proxy <- leafletProxy("Map")
+    if (input$show_communities) {
+      proxy %>% showGroup("locations")
+    } else {
+      updateSelectInput(session, "location", selected="")
+      proxy %>% hideGroup("locations") %>% removeMarker(layerId="Selected")
+    }
+  })
+
+  observeEvent(input$Map_marker_click, { # update the map markers and view on map clicks
+    p <- input$Map_marker_click
+    proxy <- leafletProxy("Map")
+    if(p$id=="Selected"){
+      proxy %>% removeMarker(layerId="Selected")
+    } else if(p$group=="locations") {
+      proxy %>% setView(lng=p$lng, lat=p$lat, input$Map_zoom) %>% acm_defaults(p$lng, p$lat)
+    }
+  })
+
+  observeEvent(input$Map_marker_click, { # update the location selectInput on map clicks
+    p <- input$Map_marker_click
+    if(!is.null(p$id)){
+      if(is.null(input$location) || input$location!=p$id) updateSelectInput(session, "location", selected=p$id)
+    }
+  })
+
+  observeEvent(input$location, { # update the map markers and view on location selectInput changes
+    p <- input$Map_marker_click
+    p2 <- subset(locs, loc==input$location)
+    proxy <- leafletProxy("Map")
+    if(nrow(p2)==0){
+      proxy %>% removeMarker(layerId="Selected")
+    } else if(length(p$id) && input$location!=p$id){
+      proxy %>% setView(lng=p2$lon, lat=p2$lat, input$Map_zoom) %>% acm_defaults(p2$lon, p2$lat)
+    } else if(!length(p$id)){
+      proxy %>% setView(lng=p2$lon, lat=p2$lat, input$Map_zoom) %>% acm_defaults(p2$lon, p2$lat)
+    }
+  })
+  #### END Map-related observers ####
+
+  # Location data
+  Loc_Var <- reactive({ vars[var.labels==input$loc_variable] })
+
+  Loc_RCPs <- reactive({ rcps[rcp.labels==input$loc_rcp] })
+
+  Data <- reactive({
+    x <- filter(d.gcm, Var==Loc_Var() & RCP==Loc_RCPs()) %>% do(
+      Locs=.$Locs[[1]] %>% purrr::map2(month.abb, ~mutate(.x, Month=factor(.y, levels=month.abb))) %>%
+        bind_rows %>% filter(Location==input$location)) %>% unnest
+    x <- d.cru %>% do(Locs=filter(.$Locs[[1]], Location==input$location)) %>% unnest %>%
+      filter(Var==Loc_Var()) %>% mutate(RCP="historical", Model="CRU 3.2") %>% bind_rows(x) %>%
+      select(Location, Var, RCP, Model, Year, Month, value) %>%
+      mutate(Model=factor(Model, levels=unique(Model))) %>%
+      group_by(Location, Var, RCP, Model, Month)
+    x
+  })
+
+  Data_sub <- reactive({ # subset data
+    p <- input$loc_toy
+    monthly <- p %in% month.abb
+    mos <- if(monthly) p else month.abb[sea.idx[[input$loc_toy]]]
+    x <- Data() %>% filter(Month %in% mos)
+    if(monthly){
+      x <- group_by(x)
+    } else {
+      f_sea <- if(Loc_Var()=="pr") sum else mean
+      rnd <- ifelse(Loc_Var()=="pr", 0, 1)
+      if(p=="Winter"){
+        x <- mutate(x, PrevYear=c(NA, value[1:(length(value)-1)])) %>%
+          mutate(value=ifelse(Month=="Dec", PrevYear, value))
+      }
+      x <- mutate(x, Season=factor(season.labels.long[match(Month, month.abb)], levels=season.labels)) %>%
+        group_by(Location, Var, RCP, Model, Year, Season) %>% summarise(value=f_sea(value)) %>% group_by %>%
+        mutate(value=round(value, rnd))
+    }
+    x
+  })
+
+  Data_sub2 <- reactive({ # transform to deltas and/or remove CRU 3.2 if required
+    x <- Data_sub()
+    cru.mean <- (filter(x, Model=="CRU 3.2" & Year %in% 1961:1990) %>% summarise(Mean=mean(value)))$Mean
+    if(input$loc_deltas){
+      x <- group_by(x, Model)
+      if(Loc_Var()=="pr"){
+         x <- mutate(x, value=round(value / cru.mean, 2))
+        #x[is.infinite(x)] <- NA
+      } else {
+        x <- mutate(x, value=round(value - cru.mean, 2))
+      }
+    }
+    if(!input$loc_cru) x <- filter(x, Model!="CRU 3.2")
+    x
+  })
+
+  loc_data_filename <- reactive({ # input$gbm_plottype and GBM_Region2() don't update from with server-side UI selectInput
+    #paste0("gbmResults_", gsub(" ", "", input$gbm_plottype), "_", gsub(" ", "", GBM_Region()), gsub(" ", "", GBM_Region2()), ".pdf")
+    "current_plot_data.csv"
+  })
+
+  # Outputs for location modal
+  output$dl_loc_data <- downloadHandler(
+    filename=loc_data_filename(),
+    content=function(file){	write.csv(Data_sub2(), file, quote=FALSE, row.names=FALSE) }
+  )
+
+  do_tsplot <- reactive({
+    req(input$modal_loc)
+    if(!input$modal_loc) return()
+    d <- Data_sub2()
+    d2 <- filter(d, Model!="CRU 3.2")
+    p <- Loc_Var()=="pr"
+    del <- input$loc_deltas
+    clrs <- c("darkgray", "cornflowerblue", "orange", "purple", "dodgerblue4", "firebrick")
+    if(!input$loc_cru){
+      clrs <- clrs[-1]
+      d <- d2
+    }
+    ylb <- if(p & del) "Precipitation deltas" else if(p) "Precipitation (mm)" else if(!p & del) "Temperature deltas (C)" else "Temperature (C)"
+    g <- ggplot(d, aes(Year, value, colour=Model)) + scale_color_manual("", values=clrs) + labs(y=ylb) +
+      theme_gray(base_size=16) + theme1
+    if(input$loc_stat=="All GCMs") g <- g + geom_line()
+    if(input$loc_stat=="Both"){
+      g <- g + geom_line() + stat_summary(data=d2, aes(colour=NULL), fun.y=mean, geom="line", colour="black", size=1)
+    } else if(input$loc_stat=="Mean GCM"){
+      if(input$loc_cru) g <- g + geom_line(data=filter(d, Model=="CRU 3.2"))
+      g <- g + stat_summary(data=d2, aes(colour=NULL), fun.y=mean, geom="line", colour="black", size=1)
+    }
+    if(input$loc_trend) g <- g + geom_smooth(aes(colour=NULL), colour="black", size=1)
+    g
+  })
+
+  output$TS_Plot <- renderPlot({ do_tsplot() })
+
+  tsplot_filename <- reactive({ # input$gbm_plottype and GBM_Region2() don't update from with server-side UI selectInput
+    #paste0("gbmResults_", gsub(" ", "", input$gbm_plottype), "_", gsub(" ", "", GBM_Region()), gsub(" ", "", GBM_Region2()), ".pdf")
+    "current_plot.pdf"
+  })
+
+  tsplot_h <- reactive({ session$clientData$output_TS_Plot_height })
+  tsplot_w <- reactive({ session$clientData$output_TS_Plot_width })
+
+  output$dl_tsplot <- downloadHandler(
+    filename=tsplot_filename(),
+    content=function(file){	pdf(file=file, width=15, height=15*tsplot_h()/tsplot_w(), pointsize=6); print(do_tsplot()); dev.off() }
+  )
+
+  # Spatial distribution density plot
+  output$sp_density_plot <- renderPlot({
+    x <- ras_vals()
+    x <- data.table(`Spatial Distribution`=x[!is.na(x)])
+    ggplot(x, aes(`Spatial Distribution`)) + geom_density(fill="#33333350") + theme1 + labs(x=Legend_Title())
+  }, width=300, height=300, bg="transparent")
+
+  observe({ # no deltas allowed when comparing across models
+    if(!(input$mod_or_stat %in% models)){
+      updateCheckboxInput(session, "deltas", value=FALSE)
+    }
+  })
+
+  # tooltips
+  observe({
+    if(length(input$ttips) && input$ttips){
+      addTooltip(session, "dec", "Maps show decadal averages of projected climate.", "left", options=list(container="body"))
+      addTooltip(session, "toy", "3-month seasons or specific months. Winter is Dec-Feb and so on.", "left", options=list(container="body"))
+      addTooltip(session, "rcp", "Representative Concentration Pathways, covering a range of possible future climates based on atmospheric greenhouse gas concentrations.", "left", options=list(container="body"))
+      addTooltip(session, "mod_or_stat", "Individual climate models or a statistic combining all five.", "left", options=list(container="body"))
+      addTooltip(session, "location", "Enter a community. Menu filters as you type. Or select a community on map.", "left", options=list(container="body"))
+      addTooltip(session, "deltas", "Display projected change from 1961-1990 baseline average instead of raw climate values.", "right", options=list(container="body"))
+      addTooltip(session, "show_communities", "Clickable locations reveal special app-provided community data.
+        These are different from map markers showing user-uploaded shapefile points.", "right", options=list(container="body"))
+      addTooltip(session, "lat_range", "If cropped to a rectangle with insufficient data, the map will revert to its full extent.", "left", options=list(container="body"))
+      addTooltip(session, "btn_modal_shp", "Upload a polygon shapefile for arbitrary masking.
+        Once uploaded, the mask can be toggled on or off and may be combined with lon/lat sliders. Using a mask adds some delay.", "bottom", options=list(container="body"))
+    } else {
+      removeTooltip(session, "dec")
+      removeTooltip(session, "toy")
+      removeTooltip(session, "rcp")
+      removeTooltip(session, "mod_or_stat")
+      removeTooltip(session, "location")
+      removeTooltip(session, "deltas")
+      removeTooltip(session, "show_communities")
+      removeTooltip(session, "lat_range")
+      removeTooltip(session, "btn_modal_shp")
+    }
+  })
+
+})
---
+++ nwtapp/ui.R
@@ -0,0 +1,109 @@
+source("about.R", local=T)
+
+# for shinyapps.io, use theme="spacelab.css" with file in www folder.
+# for local/RStudio and shiny-server, use theme="http://bootswatch.com/spacelab/bootstrap.css" (this is ignored on shinyapps.io)
+# shinytheme() from shinythemes package must be avoided because it conflicts with bsModal in shinyBS.
+
+shinyUI(navbarPage(theme="http://bootswatch.com/spacelab/bootstrap.css", inverse=TRUE,
+  title=HTML('<div><a href="http://snap.uaf.edu" target="_blank"><img src="./img/SNAP_acronym_100px.png" width="100%"></a></div>'),
+  windowTitle="NT Climate",
+  collapsible=TRUE,
+  id="nb",
+  tabPanel("Climate", value="vis",
+  tags$head(includeScript("ga-nwtapp.js"), includeScript("ga-allapps.js")),
+  shpPolyInput("user_shapefile", "Upload polygon shapefile", "btn_modal_shp"),
+  bsModal("modal_loc", "Community Insights", "btn_modal_loc", size = "large",
+    fluidRow(
+      column(3,
+        selectInput("loc_variable", "", var.labels, var.labels[1]),
+        checkboxInput("loc_deltas", "Display deltas", FALSE)
+      ),
+      column(3,
+        selectInput("loc_rcp", "", rcp.labels, rcp.labels[1]),
+        checkboxInput("loc_cru", "Show historical", FALSE)
+      ),
+      column(3,
+        selectInput("loc_stat", "", c("All GCMs", "Mean GCM", "Both"), "All GCMs"),
+        checkboxInput("loc_trend", "Smooth trend", FALSE)
+      ),
+      column(3,
+        selectInput("loc_toy", "", toy_list, toy_list[[1]][1])
+      )
+    ),
+    plotOutput("TS_Plot"),
+    fluidRow(
+      column(3, downloadButton("dl_tsplot", "Get Plot", class="btn-block")),
+      column(3, downloadButton("dl_loc_data", "Get Data", class="btn-block"))
+    )
+  ),
+  div(class="outer",
+  tags$head(includeCSS("www/styles.css")),
+  leafletOutput("Map", width="100%", height="100%"),
+  absolutePanel(top=20, left=60, height=20, width=600, h4("Northwest Territories Future Climate Outlook")),
+  absolutePanel(id="controls", top=20, right=-10, height=200, width=400,
+    sliderInput("dec", "Decade", min=min(decades), max=max(decades), value=max(decades), step=10, sep="", post="s", width="100%"),
+    wellPanel(
+      fluidRow(
+        column(6,
+          selectInput("toy", "Time of year", toy_list, toy_list[[1]][1]),
+          selectInput("rcp", "RCP", rcp.labels, rcp.labels[1])
+        ),
+        column(6,
+          selectInput("variable", "Variable", var.labels, var.labels[1]),
+          selectInput("mod_or_stat", "GCM data", maptype_list, maptype_list[[1]][1])
+        )
+      ),
+      conditionalPanel("input.show_communities == true",
+        selectInput("location", "Community", c("", locs$loc), selected="", width="100%"),
+          conditionalPanel("input.location !== null && input.location !== ''",
+            actionButton("btn_modal_loc", "Community Insights", class="btn-block"))
+      )
+    ),
+    conditionalPanel("input.show_extent == true",
+      fluidRow(
+        column(6, sliderInput("lon_range", "Longitude", min=ext[1], max=ext[2], value=ext[1:2], step=1, sep="", post="°", width="100%")),
+        column(6, sliderInput("lat_range", "Latitude", min=ext[3], max=ext[4], value=ext[3:4], step=1, sep="", post="°", width="100%"))
+      ),
+      fluidRow(
+        column(6,
+          actionButton("btn_modal_shp", "Upload shapefile", class="btn-block"),
+          uiOutput("Shp_On")
+        ),
+        column(3, h4("Mask:")),
+        column(3, uiOutput("Mask_in_use"))
+      )
+    ),
+    fluidRow(column(12, downloadButton("dl_raster", "Get Map (.tif)", class="btn-block")))
+  ),
+  absolutePanel(id="controls", top=60, left=-20, height=300, width=300,
+    plotOutput("sp_density_plot", width="100%", height="auto")
+  ),
+  absolutePanel(bottom=10, left=10,
+    conditionalPanel(is_gcm_string, checkboxInput("deltas", "Display deltas", FALSE)),
+    checkboxInput("show_communities", "Communities", TRUE),
+    checkboxInput("show_extent", "Crop/mask", FALSE),
+    checkboxInput("show_colpal", "Color options", FALSE),
+    checkboxInput("legend", "Legend", TRUE),
+    checkboxInput("ttips", "Tooltips", FALSE)
+  ),
+  bsTooltip("ttips", "Toggle tooltips like this one for other app controls.", "right", options=list(container="body")),
+  absolutePanel(id="controls", bottom=240, left=-10, height=190, width=320,
+    conditionalPanel("input.show_colpal == true",
+    wellPanel(
+      fluidRow(
+        column(6, selectInput("colpal", "Palette", colpals_list, colpals_list[[1]][1])),
+        column(6, conditionalPanel("input.colpal == 'Custom div'", colourInput("col_med", "Med", value = "#CEEBF0")))
+      ),
+      fluidRow(
+        column(6, conditionalPanel("input.colpal == 'Custom div' || input.colpal == 'Custom seq'",
+          colourInput("col_low", "Low", value = "#000470"))),
+        column(6, conditionalPanel("input.colpal == 'Custom div' || input.colpal == 'Custom seq'",
+          colourInput("col_high", "High", value = "#8C0050")))
+      )
+    )
+    )
+  )
+  )
+  ),
+  about
+))
